10495446af7c4f8180f390704d86d3e1ff4d7776
PiotrCiba <piotr.m.ciba@gmail.com>
2023-04-02 14:07:35 +0200

    first commit, rebuilding the program in AvaloniaUI

---
+++ KlawiaturaGAvalonia/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace KlawiaturaGAvalonia.ViewModels;
+public class MainWindowViewModel : ViewModelBase
+{
+    public string KLabel { get; set; } = "Klawiatura";
+    public int KbFontSize { get; set; } = 20;
+    public string PButton { get; set; } = "Punktacja";
+    
+}
---
+++ KlawiaturaGAvalonia/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace KlawiaturaGAvalonia.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
