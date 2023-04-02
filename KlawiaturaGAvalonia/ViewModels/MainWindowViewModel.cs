ea22e222ba1ddfd3f66e40dbe1573272a56f2fec
PiotrCiba <piotr.m.ciba@gmail.com>
2023-04-02 21:50:43 +0200

    Remodelling contents from WPF xaml to AvalonioUI. Keyboard layout display done

--- KlawiaturaGAvalonia/ViewModels/MainWindowViewModel.cs
+++ KlawiaturaGAvalonia/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,13 @@
 ﻿namespace KlawiaturaGAvalonia.ViewModels;
 public class MainWindowViewModel : ViewModelBase
 {
+    //Colour fields
+    public string PrimaryColour { get; set; } = "White";
+    public string PrimKeyColour { get; set; } = "White";
+    public string SecKeyColour { get; set; } = "LightGray";
+    public string BorderColour { get; set; } = "Black";
+    
+    //UI element contents
     public string KLabel { get; set; } = "Klawiatura";
     public int KbFontSize { get; set; } = 20;
     public string PButton { get; set; } = "Punktacja";
