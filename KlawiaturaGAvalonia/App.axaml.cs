d73affee8d42e86cd2866e3fd195ab4ab518c24b
PiotrCiba <piotr.m.ciba@gmail.com>
2023-04-02 14:41:53 +0200

    am sufferin

--- KlawiaturaGAvalonia/App.axaml.cs
+++ KlawiaturaGAvalonia/App.axaml.cs
@@ -6,7 +6,7 @@ using KlawiaturaGAvalonia.Views;
 
 namespace KlawiaturaGAvalonia;
 
-public partial class App : Application
+public class App : Application
 {
     public override void Initialize()
     {
