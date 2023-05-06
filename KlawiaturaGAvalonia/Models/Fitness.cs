fe187f1ef77074c9580405cfdd64d06cd3e02e99
PiotrCiba <piotr.m.ciba@gmail.com>
2023-05-07 00:01:58 +0200

    recent changes, dunno what about

---
+++ KlawiaturaGAvalonia/Models/Fitness.cs
@@ -0,0 +1,6 @@
+namespace KlawiaturaGAvalonia.Models;
+
+public class Fitness
+{
+    //todo: create a fitness class for calculating a custom fitness funcion, and compare the layout against a chosen text
+}
